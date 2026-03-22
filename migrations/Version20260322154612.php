affc267248fd8bd2083a572dfc624b11b87e9636
Nikola Würknerová <nerovanikola@gmail.com>
2026-03-22 16:47:22 +0100

    Reset migrací

--- migrations/Version20260322154612.php
+++ migrations/Version20260322154612.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260318103548 extends AbstractMigration
+final class Version20260322154612 extends AbstractMigration
 {
     public function getDescription(): string
     {
